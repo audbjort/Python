cbf4b8986558577f6cdeecd38914ae8391cc63dc
Carmen <acmaxim@gmail.com>
2017-06-25 12:24:18 -0700

    Test pycharm conn.

--- Array1.py
+++ Array1.py
@@ -1,5 +1,5 @@
 # Given a sorted array of positive integers with an empty spot (zero) at the end insert an element in sorted order
-    
+
 
 from array import *
 import string
@@ -8,25 +8,25 @@ print """Given a sorted array of positive integers with an empty spot (zero) at 
 
 
 def add_element_to_array(my_array, new_element):
-  
-  my_array.append(new_element)
-  sorted_array = sorted(my_array)
-     
-  return sorted_array
-  
-  
+    my_array.append(new_element)
+    sorted_array = sorted(my_array)
+
+    return sorted_array
+
 
 def main():
-   array_ex = array('i',[1,2,5,6,7,99])
-   print 'Original array'
-   for x in array_ex:
-     print x
-  
-   print 'New array'
-   sorted_array = add_element_to_array(array_ex, 4)
-   for x in sorted_array:
-     print x 
- 
- 
+    array_ex = array('i', [1, 2, 5, 6, 7, 99])
+    print 'Original array'
+    for x in array_ex:
+        print x
+
+    print 'New array'
+    sorted_array = add_element_to_array(array_ex, 4)
+    for x in sorted_array:
+        print x
+
+    print 'Hello World'
+
+
 if __name__ == '__main__':
-  main()    
+    main()
