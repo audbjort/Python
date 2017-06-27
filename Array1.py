ee81f0820538679d2f55531e6032c430351e5b33
Carmen <acmaxim@gmail.com>
2017-06-27 09:15:05 -0700

    Test2

--- Array1.py
+++ Array1.py
@@ -4,7 +4,7 @@
 from array import *
 import string
 
-print """Given a sorted array of positive integers with an empty spot (zero) at the end insert an element in sorted order """
+print """ Given a sorted array of positive integers with an empty spot (zero) at the end insert an element in sorted order """
 
 
 def add_element_to_array(my_array, new_element):
@@ -14,18 +14,23 @@ def add_element_to_array(my_array, new_element):
     return sorted_array
 
 
+print """  Reverse the order of elements in an array (without creating a new array). """
+
+
+
 def main():
     array_ex = array('i', [1, 2, 5, 6, 7, 99])
-    print 'Original array'
+    print 'Original array: ',
     for x in array_ex:
-        print x
+        print x ,
 
-    print 'New array'
+    print '\nNew array: ',
     sorted_array = add_element_to_array(array_ex, 4)
     for x in sorted_array:
-        print x
+        print x ,
 
-    print 'Hello World'
+    print '\n\ Hello World'
+    print '\n\ Hello World'
 
 
 if __name__ == '__main__':
